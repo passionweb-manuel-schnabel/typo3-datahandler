bc769797564e4b688dad3e8b9831c8cc9e10c7ac
KrisRaszewski <Kristian@passionweb.de>
2025-10-02 11:27:29 +0200

    [TASK] Refined structure for DataHandler, ids to be edited

--- Classes/DataHandling/BasicDataHandler.php
+++ Classes/DataHandling/BasicDataHandler.php
@@ -12,7 +12,7 @@ final class BasicDataHandler
     protected DataHandler $dataHandler;
     public function __construct()
     {
-        $dataHandler = GeneralUtility::makeInstance(DataHandler::class);
+        $this->dataHandler = GeneralUtility::makeInstance(DataHandler::class);
     }
 
     public function basicUsage(): void
@@ -24,45 +24,42 @@ final class BasicDataHandler
         $this->dataHandler->process_datamap();
         $this->dataHandler->process_cmdmap();
 
-        $cmd['tt_content'][21]['delete'] = 1; //Delete record with uid 21
-        $cmd['tt_content'][999]['copy'] = -303; //Copy record with uid 999 directly after record with uid 303
-        $cmd['tt_content'][1000]['copy'] = 400; //Copy record with uid 1000 to first position of page with uid 400
-        $cmd['tt_content'][1000]['move'] = 400; //Move record with uid 1000 to first position of page with uid 400
+        $cmd['tt_content'][34]['delete'] = 1; //Delete record with uid 34
+        $cmd['tt_content'][25]['copy'] = -36; //Copy record with uid 25 directly after record with uid 36
+        $cmd['tt_content'][27]['copy'] = 1; //Copy record with uid 26 to first position of page with uid 1
+        $cmd['tt_content'][35]['move'] = 1; //Move record with uid 35 to first position of page with uid 1
 
         $this->dataHandler->start([], $cmd);
         $this->dataHandler->process_cmdmap();
 
-        $uid = $this->dataHandler->copyMappingArray_merged['tt_content'][1000];
+        $uid = $this->dataHandler->copyMappingArray_merged['tt_content'][27];
 
         $errors = $this->dataHandler->errorLog;
 
-        //Codebreak 2
         $data['pages']['NEW12345'] = [
             'title' => 'New Codebreak Page',
-            'pid' => '123',
+            'pid' => '1',
         ];
         $data['pages']['NEW54321'] = [
-            'title' => 'Subpage after Codebreak Page',
-            'pid' => '-123',
+            'title' => 'Subpage',
+            'pid' => '-16',
         ];
         $data['pages']['NEW11111'] = [
             'title' => 'Another Subpage after Codebreak Page',
-            'pid' => -'NEW54321',
+            'pid' => '-NEW54321',
         ];
 
         $this->dataHandler->start($data, []);
         $this->dataHandler->process_datamap();
 
-        $data['sys_categories']['NEW99999'] = [
+        $data['sys_category']['NEW99999'] = [
             'title' => 'New Category',
-            'pid' => 100
+            'pid' => 1
         ];
-        $data['pages']['NEW12345'] = [
-            'title' => 'New Codebreak Page',
-            'pid' => '123',
-            'categories' => [
-                'NEW99999'
-            ]
+        $data['tt_content']['NEWab9999'] = [
+            'header' => 'Working with Categories',
+            'pid' => 1,
+            'categories' => 'NEW99999'
         ];
 
         $newId = $this->dataHandler->substNEWwithIDs['NEW12345'];
@@ -70,9 +67,9 @@ final class BasicDataHandler
             'title' => 'Edited Title',
             'no_cache' => 1
         ];
+
         $this->dataHandler->start($data, []);
         $this->dataHandler->process_datamap();
-
         $this->dataHandler->clear_cacheCmd("all");
     }
 }
