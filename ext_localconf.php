b103d158b6c3762ed467cde3d0792941c69d0275
Manuel Schnabel <mschnabel1992@gmail.com>
2025-11-27 15:08:02 +0100

    [TASK] remove previous generated hooks

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,10 +5,6 @@ declare(strict_types=1);
 use Passionweb\DataHandler\Hooks\DataHandler\AfterAllOperations;
 use Passionweb\DataHandler\Hooks\DataHandler\AfterDatabaseAction;
 use Passionweb\DataHandler\Hooks\DataHandler\AfterFinish;
-use Passionweb\DataHandler\Hooks\DataHandler\BeforeStart;
-use Passionweb\DataHandler\Hooks\DataHandler\PostProcess;
-use Passionweb\DataHandler\Hooks\DataHandler\PreProcess;
-use Passionweb\DataHandler\Hooks\DataHandler\ProcessCmdmap;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die();
@@ -24,9 +20,5 @@ ExtensionUtility::configurePlugin(
     ]
 );
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['AfterAllOperations'] =
-    AfterAllOperations::class;
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['AfterDatabaseAction'] =
-    AfterDatabaseAction::class;
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['AfterFinish'] =
-    AfterFinish::class;
+
+
