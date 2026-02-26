e46bf5f766a2049d644f85fd4c6d9d43436362ad
KrisRaszewski <Kristian.Raszewski@outlook.de>
2026-02-26 11:35:23 +0100

    [TASK] removed unneeded typoscript

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,8 +6,6 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:data_handler/Configuration/TsConfig/TCEFORM.tsconfig">');
-
 ExtensionUtility::configurePlugin(
     'DataHandler',
     'DataHandler',
